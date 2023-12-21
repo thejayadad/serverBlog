95786f2b0f21e84a3f8a933fa8c149e602299335
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-12-21 09:16:22 -0500

    update

--- myapp/app/account/layout.js
+++ myapp/app/account/layout.js
@@ -1,16 +1,22 @@
+// Layout.js
 import Sidebar from '@/components/Aside/Sidebar';
+import { getServerSession } from 'next-auth';
 import React from 'react';
+import { authOptions } from '../api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
 
-const Layout = ({ children }) => {
+const Layout = async ({ children }) => {
+    const session = await getServerSession(authOptions)
+    if(!session){
+        return redirect('/')
+    }
   return (
-    <div className='sm:flex sm:flex-col flex gap-4 max-w-screen-xl mx-auto'>
-      <div className='grid sm:grid sm:grid-cols-1 md:grid-cols-6 lg:grid-cols-8 w-full'>
-        <div className='col-span-2 w-full p-24'>
-          <Sidebar />
-        </div>
-        <div className='col-span-6 bg-red-400 w-full p-24'>
-          {children}
-        </div>
+    <div className='flex flex-col sm:flex-row gap-4 max-w-screen-xl mx-auto'>
+      <div className='sm:w-2/5 p-4'>
+        <Sidebar />
+      </div>
+      <div className='sm:w-3/5 bg-red-400 p-4'>
+        {children}
       </div>
     </div>
   );
